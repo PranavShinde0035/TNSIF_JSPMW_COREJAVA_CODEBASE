27ca5dabae10b1bb7c52895c7c3712dd9381063f
Pranav Shinde <pranavshinde735053@gmail.com>
2025-08-01 13:21:50 +0530

    New

---
+++ TNS/src/com/tnsif/tns/identifier.java
@@ -0,0 +1,37 @@
+package com.tnsif.tns;
+
+public class identifier {
+   public static void main(String[] args) {
+	   //initialization of variable
+	   int $value=35;
+	   System.out.println("Stored number is:"+$value);
+	   
+	   String studentName="PranavShinde";
+	   System.out.println("Stored name is:"+studentName);
+	   
+	   int value1=100/3;
+	   System.out.println("Output is:"+value1);
+	   
+	   float value2=100f/3f;
+	   System.out.println("Output is:"+value2);
+	   
+	   double value3=100d/3d;
+	   System.out.println("Output is:"+value3);
+	   
+	   char ch='x';
+	   System.out.println(ch);
+	   
+	   char ch1=65;
+	   System.out.println(ch1);
+	   
+	   int a='A';
+	   System.out.println(a);
+	   
+	   long b=1000000000000000000l;
+	   System.out.println(b);
+	   
+	   byte byteMax=127;
+	   byte byteMin=-128;
+	   
+   }
+}
